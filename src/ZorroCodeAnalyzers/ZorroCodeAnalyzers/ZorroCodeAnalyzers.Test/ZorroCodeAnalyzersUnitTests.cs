0cd5c4ffaed466af4642bd46c4c9b00b193bffdf
KULBIDA Pavel <Pavel.A.KULBIDA@raiffeisen.ru>
2022-09-16 18:53:43 +0600

    load solution

---
+++ src/ZorroCodeAnalyzers/ZorroCodeAnalyzers/ZorroCodeAnalyzers.Test/ZorroCodeAnalyzersUnitTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using VerifyCS = ZorroCodeAnalyzers.Test.CSharpCodeFixVerifier<
+    ZorroCodeAnalyzers.ZA0001FeaturesIntersector,
+    ZorroCodeAnalyzers.ZA0001FeaturesIntersectorCodeFixProvider>;
+
+namespace ZorroCodeAnalyzers.Test
+{
+  [TestClass]
+  public class ZorroCodeAnalyzersUnitTest
+  {
+    //No diagnostics expected to show up
+    [TestMethod]
+    public async Task EmptyTest()
+    {
+      var test = @"";
+
+      await VerifyCS.VerifyAnalyzerAsync(test);
+    }
+
+    [TestMethod]
+    public async Task HappyTest()
+    {
+      var test = @"
+        using FeaturesAnalyzer.Debug.Features.FeatureOne;
+
+        namespace FeaturesAnalyzer.Debug.Features.FeatureTwo
+        {
+          class Buzz
+          {
+            public Fizz Value { get; set; }
+          }
+        }
+      ";
+
+      var expected1 = DiagnosticResult.CompilerError("ZA0001").WithSpan(2, 15, 2, 57).WithArguments("FeatureTwo", "FeatureOne");
+      var expected2 = DiagnosticResult.CompilerError("CS0234").WithSpan(2, 47, 2, 57).WithArguments("FeatureOne", "FeaturesAnalyzer.Debug.Features");
+      var expected3 = DiagnosticResult.CompilerError("CS0246").WithSpan(8, 20, 8, 24).WithArguments("Fizz");
+
+      await VerifyCS.VerifyAnalyzerAsync(test, expected1, expected2, expected3);
+    }
+
+
+    //Diagnostic and CodeFix both triggered and checked for
+    //[TestMethod]
+    public async Task TestMethod2()
+    {
+      var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+      var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+      var expected = VerifyCS.Diagnostic("ZorroCodeAnalyzers").WithLocation(0).WithArguments("TypeName");
+      await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+    }
+  }
+}
---
+++ src/ZorroCodeAnalyzers/ZorroCodeAnalyzers/ZorroCodeAnalyzers/ZA0003ApplicationIntersector.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace ZorroCodeAnalyzers
+{
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class ZA0003ApplicationIntersector : DiagnosticAnalyzer
+  {
+    public const string DiagnosticId = "ZA0003";
+    private const string Category = "Architecture";
+    private const string KeyWord = "Application";
+
+    private static readonly string[] rejectedKeyWord = { "Infrastructure" };
+
+    private static readonly LocalizableString title = new LocalizableResourceString(nameof(Resources.ZA0003Title), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString messageFormat = new LocalizableResourceString(nameof(Resources.ZA0003MessageFormat), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString description = new LocalizableResourceString(nameof(Resources.ZA0003Description), Resources.ResourceManager, typeof(Resources));
+
+    private static readonly DiagnosticDescriptor rule = new DiagnosticDescriptor(DiagnosticId, title, messageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: description);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+      context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+      context.EnableConcurrentExecution();
+
+      context.RegisterSyntaxTreeAction(AnalyzeContext);
+    }
+
+    private static void AnalyzeContext(SyntaxTreeAnalysisContext context)
+    {
+      var root = context.Tree.GetCompilationUnitRoot();
+
+      var namespaceName = root.Members
+        .Where(x => x is NamespaceDeclarationSyntax)
+        .Select(x => ((NamespaceDeclarationSyntax)x).Name.ToString())
+        .FirstOrDefault();
+
+      if (string.IsNullOrEmpty(namespaceName))
+      {
+        return;
+      }
+
+      var keyWordPosition = GetKeyWordPosition(namespaceName, KeyWord);
+
+      if (keyWordPosition == -1)
+      {
+        return;
+      }
+
+      var usingNodes = root.Usings
+        .Select(x => GetPositionValue(x.Name.ToString(), keyWordPosition))
+        .ToArray();
+
+      var count = 0;
+      foreach (var usingItem in usingNodes)
+      {
+        if (usingItem != null && rejectedKeyWord.Contains(usingItem))
+        {
+          var location = root.Usings[count].Name.GetLocation();
+          var diagnostic = Diagnostic.Create(rule, location, usingItem);
+          context.ReportDiagnostic(diagnostic);
+        }
+
+        count++;
+      }
+    }
+
+    private static int GetKeyWordPosition(string route, string keyword)
+    {
+      if (string.IsNullOrEmpty(route))
+      {
+        return -1;
+      }
+
+      var segments = route.Split('.');
+
+      return Array.IndexOf(segments, keyword);
+    }
+
+    private static string GetPositionValue(string route, int keyWordPosition)
+    {
+      if (string.IsNullOrEmpty(route))
+      {
+        return null;
+      }
+
+      var segments = route.Split('.');
+
+      if (segments.Length < keyWordPosition)
+      {
+        return null;
+      }
+
+      return segments[keyWordPosition];
+    }
+  }
+}
